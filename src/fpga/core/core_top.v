206948d7f5becc0c13c31fa309e57c15ac70ee0a
Eric Lewis <ericlewis777@gmail.com>
2022-09-04 19:39:44 -0500

    hm

--- src/fpga/core/core_top.v
+++ src/fpga/core/core_top.v
@@ -446,10 +446,19 @@ core_bridge_cmd icb (
 ////////////////////////////////////////////////////////////////////////////////////////
 
 reg  [31:0]	gen_speed = 1;
+reg reset_button = 0;
+reg prev_button_15 = 0;
+
+always @(posedge clk_74a) begin
+    reset_button <= 1;
+    if (!prev_button_15 && cont1_key[15])
+      reset_button <= 0;
+    prev_button_15 = cont1_key[15];
+end
 
 life life_instance(
     .clock_74(clk_74a),
-    .reset_n(reset_n),
+    .reset_n(coin_insert),
     .x(visible_x),
     .y(visible_y),
     .r(video_rgb[23:16]),
